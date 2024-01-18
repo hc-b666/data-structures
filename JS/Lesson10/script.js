8533dd1bd5a8fe46624d0c8b9bc9a81c24b62d7b
Bobur <bobur0218programmer@gmail.com>
2024-01-18 22:45:58 +0500

    lesson10

---
+++ JS/Lesson10/script.js
@@ -0,0 +1,59 @@
+"use strict";
+// 18 january 2024
+
+// form required validation
+// button sumbit function
+
+// form -> name for inputs textarea select
+
+
+// let form;
+// form.addEventListener("submit", (event) => 
+// {
+//     event.preventDefault();
+//     let name = form.name.value;
+// })
+
+// querySelectorAll -> NodeList
+// elements will be array 
+
+// getElementByTagName -> HTMLCollection
+
+
+// getElementsByClassName -> HTMLCollection
+
+// iteration 
+
+// string and array is iterable
+
+// 1. loop
+
+// 2. forEach 
+let users = ["John", "Jack", "Jane"];
+// users.forEach((element, index) => console.log(index, element)); 
+
+// 3. map 
+// users.map((element, index) => console.log(index, element));
+// difference between forEach and map
+// forEach -> doesn't return anything
+// map -> returns new array
+
+// 4. for of
+// for (let user of users)
+// {
+//     console.log(user);
+// }
+
+// 5. for in
+for (let index in users)
+{
+    console.log(index, users[index]);
+}
+
+// difference between for of and for in
+// for of -> returns value
+// for in -> returns index
+
+
+
+
