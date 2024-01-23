682562a53808ebcf636a8226181f98e6eb203a28
Bobur <bobur0218programmer@gmail.com>
2024-01-23 22:38:42 +0500

    lesson12

---
+++ JS/Lesson12/app.js
@@ -0,0 +1,34 @@
+// array methods
+// length push pop shift unshift forEach map 
+
+// indexOf is a method that returns the index of the first occurence of the element in the array (number)
+// if the element is not found, it returns -1
+// lastIndexOf is the same but it returns the index of the last occurence of the element in the array (number)
+
+// at is a method that returns the element at the given index (number)
+// at -1 returns the last element
+
+// includes is a method that returns true if the array contains the given element (boolean)
+
+// flat is a method that returns a new array with all sub-array elements concatenated into it recursively up to the specified depth (number)
+
+// toString is a method that returns a string representing the specified array and its elements (string)
+
+// fill is a method that fills all the elements of an array from a start index to an end index with a static value (number)
+// new Array() creates an empty array with the given length (number)
+
+// concat is a method that returns a new array comprised of this array joined with other array(s) and/or value(s) (array)
+// it can add itself
+
+// reverse is a method that reverses an array in place (array)
+
+// join is a method that joins all elements of an array into a string (string)
+
+// slice is a method that returns a shallow copy of a portion of an array into a new array object selected from begin to end (end not included) (array)
+
+// splice is a method that changes the contents of an array by removing or replacing existing elements and/or adding new elements in place (array)
+
+// mutable vs immutable
+
+
+
