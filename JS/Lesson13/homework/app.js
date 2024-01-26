161e59db99dd7cc9613ce17d40f6e4cc2c17e682
Bobur <bobur0218programmer@gmail.com>
2024-01-26 11:06:11 +0500

    new features

--- JS/Lesson13/homework/app.js
+++ JS/Lesson13/homework/app.js
@@ -1,57 +1,79 @@
 let productsArray = 
 [
     {   
-        title: "T-Shirts",
+        type: "T-Shirts",
+        brand: "Uniqlo",
         imgSource: "./images/tshirts-men1.jpg",
+        price: 5,
     },
     {   
-        title: "T-Shirts",
+        type: "T-Shirts",
+        brand: "Mott",
         imgSource: "./images/tshirts-men2.jpg",
+        price: 10,
     },
     {   
-        title: "Jeans",
+        type: "Jeans",
+        brand: "Denim",
         imgSource: "./images/jeans-men1.jpg",
+        price: 40,
     },
     {   
-        title: "Sneakers",
+        type: "Sneakers",
+        brand: "New Balance",
         imgSource: "./images/sneakers-men1.jpg",
+        price: 33,
     },
     {   
-        title: "Jeans",
+        type: "Jeans",
+        brand: "Denim",
         imgSource: "./images/jeans-men2.jpg",
+        price: 44,
     },
     {   
-        title: "T-Shirts",
+        type: "T-Shirts",
+        brand: "Uniqlo",
         imgSource: "./images/tshirts-men3.jpg",
+        price: 7,
     },
     {   
-        title: "Sneakers",
+        type: "Sneakers",
+        brand: "Balenciaga",
         imgSource: "./images/sneakers-men2.jpg",
+        price: 77,
     },
     {   
-        title: "Sneakers",
+        type: "Sneakers",
+        brand: "Air Jordan",
         imgSource: "./images/sneakers-men3.jpg",
+        price: 115,
     },
     {   
-        title: "Jeans",
+        type: "Jeans",
+        brand: "Mott",
         imgSource: "./images/jeans-men3.jpg",
+        price: 24,
     },
 ];
 
 let container = document.querySelector(".container");
 let userSelect = document.querySelector("select");
 let btnReset = document.getElementById("btnReset");
+let inputBrand = document.getElementById("inputBrand");
+let btnSearch = document.getElementById("btnSearch");
 
 function displayProducts (array) 
 {
     container.innerHTML = "";
-    array.forEach(item => 
+    array.forEach(product => 
     {
         let card = 
         `<div class="cards">
-            <img src="${item.imgSource}">
+            <img src="${product.imgSource}">
             <div class="content">
-                <h1>${item.title}</h1>
+                <h1>${product.type}</h1>
+                <h1>${product.brand}</h1>
+                <h1>$${product.price}</h1>
             </div>
         </div>`;
         container.insertAdjacentHTML("beforeend", card);
@@ -61,14 +83,31 @@ function displayProducts (array)
 function userSelectDisplay (event) 
 {
     let selectedValue = event.target.value;
-    let filteredProductsArray = productsArray.filter(item => item.title === selectedValue);
+    let filteredProductsArray = productsArray.filter(product => product.type === selectedValue);
 
     displayProducts(filteredProductsArray);
 }
 
+function userInputSearch () 
+{
+    let sortedArray = [];
+
+    let brandValue = inputBrand.value;
+    brandValue = brandValue.trim().replaceAll(" ", "").toLowerCase();
+
+    productsArray.forEach(product => 
+    {
+        let brand = product.brand.trim().replaceAll(" ", "").toLowerCase();
+        if (brand.includes(brandValue)) 
+        {
+            sortedArray.push(product);
+        } 
+    });
+    displayProducts(sortedArray);
+}
+
 displayProducts(productsArray);
-
 userSelect.addEventListener("change", userSelectDisplay);
-
 btnReset.addEventListener("click", () => displayProducts(productsArray));
+btnSearch.addEventListener("click", userInputSearch);
 
