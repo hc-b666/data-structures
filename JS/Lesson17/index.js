98f5813e0ca950d945652c6a0a285c0c29ae0a2e
Bobur <bobur0218programmer@gmail.com>
2024-02-03 23:03:32 +0500

    lesson17

---
+++ JS/Lesson17/index.js
@@ -0,0 +1,44 @@
+// February 3, 2024
+"use strict";
+
+const qs = tag => document.querySelector(tag);
+const getID = tag => document.getElementById(tag);
+const createEl = tag => document.createElement(tag);
+
+window.addEventListener("DOMContentLoaded", function ()
+{
+
+});
+
+// MATH OBJECT METHODS
+// Math.round(1.5)  =2
+// Math.floor(1.99)  =1
+// Math.ceil(1.1)  =2
+// Math.abs(-1)  =1
+// Math.PI  =3.14
+// 2**5  =32
+// Math.pow(2, 5)  =32
+// Math.sqrt(64)  =8
+// Math.trunc(1.9)  =1
+// Math.min(1, 2, 3)  =1
+// Math.max(1, 2, 3)  =3
+// ... spread operator array without []
+// Math.random() - random number between 0-1
+// Math.round(Math.random() * (max - min) + min) 
+
+// OBJECT METHODS
+// object['key'] === object.key
+// Object.keys(object) array of keys
+// Object.values(object) array of values
+// Object.entries(object)
+
+// Optional Chaining   ?
+
+// OBJECT DESTRUCTION
+// let { key1, key2, object:{ key1, key2 } } = object
+
+// ARRAY DESTRUCTION
+// [index1, index2, index3] = array
+
+// new Set() add size has delete  clear
+
