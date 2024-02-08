e22d060da9ffb24138d457bcb4fa5432f0b7e1ef
Bobur <bobur0218programmer@gmail.com>
2024-02-08 11:22:01 +0500

    fixed some bugs added new features but dunno how they are working

--- JS/Lesson18/hw/index.js
+++ JS/Lesson18/hw/index.js
@@ -1,24 +1,34 @@
 "use strict";
 
+const createEl = tag => document.createElement(tag);
+
 const body = document.body;
 const carOrder = document.getElementById("carOrder");
+
+let carInfo;
 
 // Car Object Constructor
 function Car(name, color, price)
 {
     this.name = name;
     this.color = color;
+    this.currentColor = this.color[Object.keys(this.color)[0]];
     this.price = price;
+}
+
+Car.prototype.changeColor = function (newColor)
+{
+    this.currentColor = this.color[newColor];
 }
 
 // Car Object Array
 let carObject = 
 [
-    {name: "Damas", price: {1: 3000, 2: 5000, 3: 7000}},
-    {name: "Nexia", price: {1: 4000, 2: 6000, 3: 8000}},
-    {name: "Jentra", price: {1: 10000, 2: 12000, 3: 14000}},
-    {name: "Malibu", price: {1: 15000, 2: 17000, 3: 19000}},
-    {name: "Prado", price: {1: 20000, 2: 22000, 3: 24000}},
+    {name: "Damas", price: {1: 3000, 2: 5000, 3: 7000}, color: {black: "black", white: "white", grey: "grey", blue: "blue", red: "red"}},
+    {name: "Nexia", price: {1: 4000, 2: 6000, 3: 8000}, color: {black: "black", white: "white", grey: "grey", blue: "blue", red: "red"}},
+    {name: "Jentra", price: {1: 10000, 2: 12000, 3: 14000}, color: {black: "black", white: "white", grey: "grey", blue: "blue", red: "red"}},
+    {name: "Malibu", price: {1: 15000, 2: 17000, 3: 19000}, color: {black: "black", white: "white", grey: "grey", blue: "blue", red: "red"}},
+    {name: "Prado", price: {1: 20000, 2: 22000, 3: 24000}, color: {black: "black", white: "white", grey: "grey", blue: "blue", red: "red"}} 
 ];
 
 // Create Select Options
@@ -26,10 +36,28 @@ function createSelectOptions(selectElement, options)
 {
     options.forEach(option => 
     {
-        let optionElement = document.createElement("option");
+        let optionElement = createEl("option");
         optionElement.value = option.value;
         optionElement.text = option.text;
         selectElement.append(optionElement);
+    });
+}
+
+function formChangeColor (car)
+{
+    let options = Object.keys(car.color).map(key =>
+    {
+        return {value: key, text: `${car.color[key]}`}
+    });
+    let changeColorSelect = createEl("select");
+    changeColorSelect.name = "changeColorSelect";
+    createSelectOptions(changeColorSelect, options);
+    body.append(changeColorSelect);
+
+    changeColorSelect.addEventListener("change", () => 
+    {
+        car.changeColor(changeColorSelect.value);
+        carInfo.textContent = `You ordered a ${car.currentColor} ${car.name} for ${car.price}.`;
     });
 }
 
@@ -47,13 +75,13 @@ window.addEventListener("DOMContentLoaded", function ()
             return {value: key, text: `Option ${key}: ${selectedCar.price[key]}`};
         });
 
-        let priceSelect = document.createElement("select");
+        let priceSelect = createEl("select");
         priceSelect.name = "carPrice";
         createSelectOptions(priceSelect, options);
-        let btnOrder = document.createElement("button");
+        let btnOrder = createEl("button");
         btnOrder.textContent = "Order";
         
-        let priceForm = document.createElement("form");
+        let priceForm = createEl("form");
         priceForm.append(priceSelect, btnOrder);
         body.append(priceForm);
     
@@ -61,11 +89,12 @@ window.addEventListener("DOMContentLoaded", function ()
         {
             event.preventDefault();
             const carPrice = priceForm.carPrice.value;
-            const orderedCar = new Car(carName, carColor, selectedCar.price[carPrice]);
+            const orderedCar = new Car(carName, selectedCar.color, selectedCar.price[carPrice]);
 
-            let carInfo = document.createElement("p");
-            carInfo.textContent = `You ordered a ${orderedCar.color} ${orderedCar.name} for ${orderedCar.price}.`;
+            carInfo = createEl("p");
+            carInfo.textContent = `You ordered a ${orderedCar.currentColor} ${orderedCar.name} for ${orderedCar.price}.`;
             body.append(carInfo);
+            formChangeColor(orderedCar);
         });
     });
 });
