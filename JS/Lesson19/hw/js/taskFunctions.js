64796e1edb034ca63e2f59efbdd5810c96c8bf34
Bobur <bobur0218programmer@gmail.com>
2024-02-10 18:12:57 +0500

    task manager with some bugs

--- JS/Lesson19/hw/js/taskFunctions.js
+++ JS/Lesson19/hw/js/taskFunctions.js
@@ -3,21 +3,17 @@
 import NewTask from "./taskManager.js";
 
 const createEl = tag => document.createElement(tag);
-
 const container = document.querySelector(".tasks-container");
 
 let tasks = [];
 
-function handleTaskCheckedChange(id , isChecked)
+function handleTaskCheckedChange (id , isChecked)
 {
     let taskToUpdate = tasks.find(task => task.id === id);
-    if (taskToUpdate) 
-    {
-        taskToUpdate.completed = isChecked;
-    }
+    if (taskToUpdate) taskToUpdate.completed = isChecked;
 }
 
-function createTaskElement(id, task, completed)
+function createTaskElement (id, task, completed)
 {
     let div = createEl("div");
     div.classList.add("tasks-div");
@@ -30,13 +26,18 @@ function createTaskElement(id, task, completed)
     let taskLabel = createEl("label");
     taskLabel.textContent = task;
     taskLabel.htmlFor = `checkbox${id}`;
+    if (completed) taskLabel.classList.add("checked");
+    taskCheckBox.addEventListener("change", function () 
+    {
+        handleTaskCheckedChange(id, this.checked);
+        taskLabel.classList.toggle("checked");
+    });
 
     let btnSetting = createEl("button");
     let settingIcon = createEl("i");
     settingIcon.className = "fa-solid fa-ellipsis";
     btnSetting.classList.add("btn-setting");
     btnSetting.append(settingIcon);
-
     btnSetting.addEventListener("click", (event) => 
     {
         event.stopPropagation();
@@ -44,31 +45,21 @@ function createTaskElement(id, task, completed)
         div.append(settingsContainer);
     });
 
-    if (completed) taskLabel.classList.add("checked");
-
-    taskCheckBox.addEventListener("change", function () 
-    {
-        handleTaskCheckedChange(id, this.checked);
-        taskLabel.classList.toggle("checked");
-    });
-
     div.append(taskCheckBox, taskLabel, btnSetting);
-
     return div;
 }
 
-function displayAllTasks()
+function displayAllTasks (arr)
 {
-    if (!tasks.length)
+    container.innerHTML = "";
+    if (!arr.length)
     {
         let alert = createEl("p");
         alert.textContent = "No tasks to display";
         container.append(alert);
         return;
     }
-
-    container.innerHTML = "";
-    tasks.toReversed().forEach(item =>
+    arr.toReversed().forEach(item =>
     {
         let { id, task, completed } = item;
         let taskElement = createTaskElement(id, task, completed);
@@ -76,29 +67,48 @@ function displayAllTasks()
     });
 }
 
-function setting(id)
+function deleteTask (id)
+{
+    let indexToDelete = tasks.findIndex(task => task.id === id);
+    if (indexToDelete !== -1)
+    {
+        tasks.splice(indexToDelete, 1);
+        displayAllTasks(tasks);
+    }        
+}
+
+function editTask (id)
+{
+    let taskToEdit = tasks.find(task => task.id === id);
+    if (taskToEdit) 
+    {
+        let newTaskName = window.prompt("Enter new task name", taskToEdit.task);
+        if (newTaskName) 
+        {
+            taskToEdit.task = newTaskName;
+            displayAllTasks(tasks);
+        }
+    }
+}
+
+function setting (id)
 {
     let settingsContainer = createEl("div");
     settingsContainer.classList.add("setting-container");
 
-    let btnDelete = createEl("button");
-    btnDelete.textContent = "Delete";
-
-    btnDelete.addEventListener("click", function () 
-    {
-        let indexToDelete = tasks.findIndex(task => task.id === id);
-        if (indexToDelete !== -1)
-        {
-            tasks.splice(indexToDelete, 1);
-            displayAllTasks();
-        }
-    });
-
     let btnEdit = createEl("button");
     btnEdit.textContent = "Edit";
+    btnEdit.addEventListener("click", () => editTask(id));
+
+    let btnDelete = createEl("button");
+    btnDelete.textContent = "Delete";
+    btnDelete.addEventListener("click", (event) => 
+    {
+        event.stopPropagation();
+        deleteTask(id);
+    }); 
 
     settingsContainer.append(btnEdit, btnDelete);
-
     document.addEventListener("click", function (event) 
     {
         if (!settingsContainer.contains(event.target))
@@ -106,21 +116,39 @@ function setting(id)
             settingsContainer.classList.add("not-visible");
         }
     });
-
     return settingsContainer;
 }
 
-function addTask(event)
+function addTask (event)
 {
     event.preventDefault();
 
     let taskValue = taskForm.newTask.value;
-
     let newTask = new NewTask((tasks.length + 1), taskValue, false);
     newTask.storeTask(tasks);
-    displayAllTasks();
-
+    displayAllTasks(tasks);
     taskForm.newTask.value = "";
 }
 
-export { addTask, displayAllTasks };
+function clearTasks ()
+{
+    tasks.length = 0;
+    container.innerHTML = "";
+    displayAllTasks(tasks);
+}
+
+function filterDoingTasks ()
+{
+    let doingTasks = tasks.filter(task => task.completed === false);
+    container.innerHTML = "";
+    displayAllTasks(doingTasks);
+}
+
+function filterDoneTasks ()
+{
+    let doneTasks = tasks.filter(task => task.completed === true);
+    container.innerHTML = "";
+    displayAllTasks(doneTasks);
+}
+
+export { addTask, displayAllTasks, clearTasks, filterDoingTasks, filterDoneTasks, tasks };
