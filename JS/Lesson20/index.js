34547c91ce70a66ce5bdddb9d0bee72522c4b05a
Bobur <bobur0218programmer@gmail.com>
2024-02-13 22:37:14 +0500

    lesson21

---
+++ JS/Lesson20/index.js
@@ -0,0 +1,7 @@
+// Lesson 20 
+// Topic: Async/Await
+// These are keywords that allow you to write asynchronous code in a synchronous manner.
+// async: This keyword is used to declare an asynchronous function. This function returns a promise. If the function throws an error, the promise will be rejected. If the function returns a value, the promise will be resolved with that value.
+// await: This keyword is used inside an async function to wait for a promise. It pauses the execution of the function and waits for the promise's resolution, and then resumes the function's execution and returns the resolved value.
+
+
