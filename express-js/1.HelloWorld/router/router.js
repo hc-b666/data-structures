dddae514cf545de68b9e41998b0d90d9d2c540e9
Bobby <bobur0218programmer@gmail.com>
2024-03-31 16:46:57 +0500

    deleted

--- express-js/1.HelloWorld/router/router.js
+++ express-js/1.HelloWorld/router/router.js
@@ -4,8 +4,8 @@ const users = require("../users.json");
 const router = express.Router();
 
 router.get("/users/:id", (req, res) => {
-    const id = req.params; // Gets the id from the url/api
-    const user = users.find(user => user.id === id.id); // Finds that user and sends plain text `Hello ${user.name}`
+    const { id } = req.params; // Gets the id from the url/api
+    const user = users.find(user => user.id === id); // Finds that user and sends plain text `Hello ${user.name}`
     if (!user) {
         return res.status(404).send("404! Not Found");
     }
